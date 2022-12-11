f90e0a013d9b4091c4931f7e9a8b2eeb8590dab1
JamesG <57632668+JimmyMcKegger@users.noreply.github.com>
2022-12-11 02:05:06 +0000

    Update Cathode.ex

--- solutions/2022/Day10/Cathode.ex
+++ solutions/2022/Day10/Cathode.ex
@@ -1,17 +1,22 @@
 defmodule Cathode do
-  # part 1
+  
   def part1(f) do
-    File.read!(f)
+    f
+    |> File.read!
     |> String.split("\n")
-    |> Enum.reduce({1, 1, 0}, &cycle(&1, &2))
+    |> Enum.reduce({1, 1, 0, ""}, &cycle(&1, &2))
+    |> IO.inspect(label: "part one")
+    |> pixel_print
   end
 
-  def cycle(line, {i, x, signal_sum}) do
+  def cycle(line, {i, x, signal_sum, pixel_string}) do
     signal_sum = signal_sum + ss_finder(i, x)
+
+    pixel_string = pixel_string <> pixelator(i, x)
 
     continue =
       if line == "noop" do
-        {i + 1, x, signal_sum}
+        {i + 1, x, signal_sum, pixel_string}
       else
         n_to_add =
           line
@@ -20,11 +25,26 @@ defmodule Cathode do
           |> String.to_integer()
 
         signal_sum = signal_sum + ss_finder(i + 1, x)
+        pixel_string = pixel_string <> pixelator(i + 1, x)
 
-        {i + 2, x + n_to_add, signal_sum}
+        {i + 2, x + n_to_add, signal_sum, pixel_string}
       end
 
     continue
+  end
+
+  def pixel_print({_i, _x, _signal_sum, pixel_string}) do
+    for <<char::binary-40 <- pixel_string>>, do: char
+  end
+
+  def pixelator(i, x) do
+    sprite = x-1..x+1
+
+    if rem(i,40) - 1 in sprite do
+      "#"
+    else
+      "."
+    end
   end
 
   def ss_finder(20, x), do: 20 * x
